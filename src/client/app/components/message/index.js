3b9069731f57a7bbeaf3ac2e719046c9ea827412
Petteri Suominen <pspfolio@gmail.com>
2017-01-01 12:37:29 +0200

    adding message component and static text

---
+++ src/client/app/components/message/index.js
@@ -0,0 +1,9 @@
+import { h, Component } from 'preact';
+
+export default class Message extends Component {
+  render() {
+    return (
+      <p>Palaveri on kustantanut 59 euroa. Se on kestänyt 00:04. Siihen osallistuu 6 henkilöä</p>
+    )
+  }
+}
--- src/client/app/index.js
+++ src/client/app/index.js
@@ -1,9 +1,15 @@
 import { h, render, Component } from 'preact';
 import Header from './components/header';
+import Message from './components/message';
 
 class App extends Component {
   render() {
-    return <Header />
+    return (
+      <div className='content'>
+        <Header />
+        <Message />
+      </div>
+    )
   }
 }
 
