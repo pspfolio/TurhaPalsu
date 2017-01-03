22c007b746e403f79a53b88ddbc59e31ffbd1023
Petteri Suominen <pspfolio@gmail.com>
2017-01-03 16:47:17 +0200

    refactoring code to be more readable

--- src/client/app/components/message/index.js
+++ src/client/app/components/message/index.js
@@ -4,21 +4,33 @@ export default class Message extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      elapsed: '00:00:00',
+      spendedMoney: 0.00
+    }
+
+    this.tick = this.tick.bind(this);
   }
 
   componentDidMount() {
-      this.updateInterval = setInterval(() => this.forceUpdate(), 1000);
+      this.updateInterval = setInterval(() => this.tick(), 500);
   }
 
   componentWillUnmount() {
     clearInterval(this.updateInterval);
   }
 
+  tick() {
+    const { handleElapsedTime, countSpendedMoney } = window.helper;
+    const elapsed = handleElapsedTime(this.props.elapsed, this.props.runningSince);
+    const spendedMoney = countSpendedMoney(this.props.elapsed, this.props.runningSince, this.props.salary);
+    this.setState({ spendedMoney, elapsed });
+  }
+
   render() {
-    const elapsed = window.helper.handleElapsedTime(this.props.elapsed, this.props.runningSince);
-    const spendedMoney = window.helper.countSpendedMoney(this.props.elapsed, this.props.runningSince, this.props.salary);
     return (
-      <p>Palaveri on kustantanut { spendedMoney } euroa. Se on kestänyt { elapsed }. Siihen osallistuu { this.props.persons } henkilöä. Keskipalkka on { this.props.salary } euroa.</p>
+      <p>Palaveri on kustantanut { this.state.spendedMoney } euroa. Se on kestänyt { this.state.elapsed }. Siihen osallistuu { this.props.persons } henkilöä. Keskipalkka on { this.props.salary } euroa.</p>
     )
   }
 }
